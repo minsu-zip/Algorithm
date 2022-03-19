54557eb3770a28d0f3adb3cf48da861d7a888df8
minsu-zip <alstn4814@gmail.com>
2022-03-20 03:25:38 +0900

    feat : 결정알고리즘(이분탐색)

---
+++ inflearn/section7(정렬,그리디,이분검색)/11.뮤직비디오.js
@@ -0,0 +1,46 @@
+// 이분 탐색 응용
+// O(nlogn)
+
+function solution(a, M) {
+  let answer = 0
+  let arr = [...a]
+
+  let start = Math.max(...arr)
+  let end = arr.reduce((a, b) => a + b, 0) // 총 노래 시간
+  let DVD = [] // DVD 용량
+
+  // 제일 긴 노래 이상 ~ 총 노래 시간 사이의 조건을 이분 탐색으로 순회
+  while (start < end) {
+    let mid = parseInt((start + end) / 2)
+    let sum = 0
+    let cnt = 0
+    arr.forEach((el) => {
+      sum += el
+      if (sum > mid) {
+        DVD.push(sum - el)
+        cnt += 1
+        sum = el
+      }
+    })
+    // 마지막 노래 카운팅
+    DVD.push(sum)
+    cnt += 1
+
+    // M과 DVD 개수가 같을 때
+    if (M === cnt) {
+      answer = Math.max(...DVD)
+      break
+      // M이 더 크면 용량을 더 작게 쪼개고 DVD 개수를 늘릴 수 있다.
+    } else if (M > cnt) {
+      end = mid - 1
+    } else {
+      start = mid + 1
+    }
+    DVD = []
+  }
+
+  return answer
+}
+
+let a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(solution(a, 3))
