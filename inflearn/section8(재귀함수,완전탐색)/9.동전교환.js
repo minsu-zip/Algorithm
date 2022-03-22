d8893b4473242ca44fe989acb60b08f2b6bc20d8
minsu-zip <alstn4814@gmail.com>
2022-03-23 03:09:57 +0900

    feat : docs

--- inflearn/section8(재귀함수,완전탐색)/9.동전교환.js
+++ inflearn/section8(재귀함수,완전탐색)/9.동전교환.js
@@ -2,8 +2,10 @@
 
 let coin = 0
 function DFS(coinSort, M, i) {
+  // 거슬러 줄 돈이 없거나 coinList를 전체 탐색 했을 경우
   if (M === 0 || i >= coinSort.length) return
 
+  // 동전보다 거슬러 줄 돈이 클 경우
   if (M >= coinSort[i]) {
     coin += parseInt(M / coinSort[i])
     M = M % coinSort[i]
@@ -14,6 +16,7 @@ function DFS(coinSort, M, i) {
 
 function solution(coinList, M) {
   let answer = 0
+  // 큰 동전 순으로 나누어 줘야지 최소 개수 구할 수 있다.
   let coinSort = [...coinList].sort((a, b) => b - a)
   DFS(coinSort, M, 0)
   answer = coin
