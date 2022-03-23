24e0e26183863cca403ec991f139bc502bddeb9f
minsu-zip <alstn4814@gmail.com>
2022-03-23 19:05:01 +0900

    refactor : 메모이제이션 사용

--- inflearn/section8(재귀함수,완전탐색)/12.조합.js
+++ inflearn/section8(재귀함수,완전탐색)/12.조합.js
@@ -1,16 +1,23 @@
 // 조합의 경우 수(메모이제이션)
 
-function recursion(n, r) {
+function recursion(n, r, memoization) {
+  if (memoization[n][r] > 0) {
+    return memoization[n][r]
+  }
+
   if (n === r || r === 0) {
     return 1
   }
 
-  return recursion(n - 1, r - 1) + recursion(n - 1, r)
+  return (memoization[n][r] =
+    recursion(n - 1, r - 1, memoization) + recursion(n - 1, r, memoization))
 }
 
 function solution(n, r) {
   let answer = 0
-  answer = recursion(n, r)
+  let memoization = Array.from(Array(n + 1), () => Array(r + 1).fill(0))
+
+  answer = recursion(n, r, memoization)
 
   return answer
 }
