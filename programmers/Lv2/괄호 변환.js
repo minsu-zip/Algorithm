22571f2efb60fb0b5b49c0da84fd7d243b2dbaa6
minsu-zip <alstn4814@gmail.com>
2022-04-23 20:44:08 +0900

    refactor🔺 : DivideUV 함수를 통해 U, V 분리

--- programmers/Lv2/괄호 변환.js
+++ programmers/Lv2/괄호 변환.js
@@ -27,6 +27,23 @@ function isBalance(s) {
   return check === 0 ? true : false
 }
 
+function DivideUV(p) {
+  let left = 0
+  let right = 0
+  let u = []
+  let v = []
+  for (let i = 0; i < p.length; i++) {
+    p[i] === '(' ? (left += 1) : (right += 1)
+    if (left === right) {
+      u = p.slice(0, i + 1)
+      v = p.slice(i + 1)
+      break
+    }
+  }
+
+  return [u, v]
+}
+
 function solution(p) {
   let answer = ''
   let strArr = p.split('')
@@ -36,31 +53,23 @@ function solution(p) {
   const rec = (p) => {
     if (p.length === 0) return ''
     let result = ''
-    let tmp = []
+    let [u, v] = DivideUV(p) // 2
 
-    while (p.length) {
-      tmp.push(p.shift())
-      // 2
-      if (isBalance(tmp)) {
-        let u = [...tmp]
-        let v = [...p]
-        // 3
-        if (isBracket(u)) {
-          result = u.join('') + rec(v)
-          break
-        }
-        // 4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다.
-        else {
-          let str = '('
-          str += rec(v) // 4-2
-          str += ')'
-          u.shift()
-          u.pop()
-          // 4-4
-          u.forEach((x) => (str += x === '(' ? ')' : '('))
-          result = str
-          break
-        }
+    if (isBalance(u)) {
+      // 3
+      if (isBracket(u)) {
+        result = u.join('') + rec(v)
+      }
+      // 4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다.
+      else {
+        let str = '('
+        str += rec(v) // 4-2
+        str += ')'
+        u.shift()
+        u.pop()
+        // 4-4
+        u.forEach((x) => (str += x === '(' ? ')' : '('))
+        result = str
       }
     }
     return result
