55ecaae21cdc2e6aa93c0e3e43a5d395c5a1303d
minsu-zip <alstn4814@gmail.com>
2022-05-01 15:48:54 +0900

    refactor : 🔺 일반 구현 로직
    body : 1. 2x2크기를 가진 보드의 시작점을 찾아 저장
    1.1. 시작점이 없는 경우 게임 종료 (0 개수 리턴)
    2. 2x2 보드를 없앤다.
    3. 정렬을 통해 값을 내려준다.

--- programmers/Lv2/[1차] 프렌즈4블록.js
+++ programmers/Lv2/[1차] 프렌즈4블록.js
@@ -1,62 +1,27 @@
-// 높이, 폭
-let M = 0
-let N = 0
-// 오른쪽, 아래, 오른쪽 아래
-const dx = [0, 1, 1]
-const dy = [1, 0, 1]
-
-// 인덱스 검증
-const isRange = (dx, dy) => {
-  if (dx >= 0 && dy >= 0 && dx < M && dy < N) {
-    return true
+// 2x2 확인
+const isCompare = (graph, i, j) => {
+  try {
+    const block = graph[i][j]
+    if (
+      block &&
+      block === graph[i + 1][j] &&
+      block === graph[i][j + 1] &&
+      block === graph[i + 1][j] &&
+      block === graph[i + 1][j + 1]
+    ) {
+      return true
+    }
+    return false
+  } catch (error) {
+    return false
   }
-  return false
 }
 
-const bfs = (graph, visited, removeBoard, i, j) => {
-  // 오른쪽, 아래, 오른쪽아래
-  const queue = [[i, j]]
-  const character = graph[i][j]
-  // 없애야하는 블록의 수 및 위치 저장
-  let result = false
-
-  while (queue.length) {
-    // 여기서 꺼낸 좌표에서 오른쪽, 왼쪽 아래, 오른쪽 아래를 전부 만족 시킬 경우만
-    // 블록 제거 즉 2x2로 붙어있는 형태가 되어야 한다.
-    const [x, y] = queue.shift()
-
-    // 2x2 붙어있는지 확인
-    let cnt = 1
-    for (let k = 0; k < 3; k++) {
-      const mx = x + dx[k]
-      const my = y + dy[k]
-      // 문자가 같은 경우 2x2 내에 범위이다
-      cnt += isRange(mx, my) && graph[mx][my] === character ? 1 : 0
-    }
-
-    // 4개인 경우 2x2로 붙어있다 -> queue 삽입, 삭제 위치저장, 방문 처리
-    if (cnt === 4) {
-      result = true
-      for (let k = 0; k < 3; k++) {
-        const mx = x + dx[k]
-        const my = y + dy[k]
-        if (visited[mx][my]) {
-          visited[mx][my] = false
-          removeBoard.push([mx, my])
-          queue.push([mx, my])
-        }
-      }
-    }
-  }
-
-  return result
-}
-
-// 블록 제거 로직
-const success = (graph) => {
-  graph.forEach((block, i) => {
-    block.forEach((item, j) => {
-      if (item === 1) {
+// 블록 제거
+const removeBoard = (graph, m, n) => {
+  for (let i = 1; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (!graph[i][j]) {
         let k = i
         while (k > 0) {
           graph[k][j] = graph[k - 1][j]
@@ -64,51 +29,45 @@ const success = (graph) => {
         }
         graph[k][j] = 0
       }
-    })
-  })
+    }
+  }
 }
 
 function solution(m, n, board) {
   // 2x2 범위에 있는 경우 1로 초기화(방문 처리)
   // 블록을 제거한 경우 빈 공간은 0으로 초기화
-  M = m
-  N = n
-  let answer = 0
+  let answer = []
   const graph = board.map((x) => x.split(''))
-  let k = 0
 
-  while (k < graph.length) {
-    const visited = Array.from(Array(M), () => Array(N).fill(true)) // 방문 여부
-    let removeBoard = []
-    // [문제 조건] 지워지는 조건에 만족하는 2×2 모양이 여러 개 있다면 한꺼번에 지워진다.
-    graph.forEach((blocks, i) => {
-      blocks.forEach((block, j) => {
-        if (typeof block === 'string') {
-          let result = bfs(graph, visited, removeBoard, i, j)
-          if (result) {
-            removeBoard.push([i, j])
-          }
-        }
-      })
-    })
-
-    if (removeBoard.length) {
-      // 방문된 위치 1로 초기화
-      while (removeBoard.length) {
-        let [x, y] = removeBoard.shift()
-        if (graph[x][y] !== 1) {
-          graph[x][y] = 1
-          answer += 1
+  while (true) {
+    let blocks = []
+    // 2x2 찾으면 시작 위치 저장
+    for (let i = 0; i < m; i++) {
+      for (let j = 0; j < n; j++) {
+        if (isCompare(graph, i, j)) {
+          blocks.push([i, j])
         }
       }
-      // 2x2가 있는 경우 블록 제거
-      // 블록 제거 후 위의 블록들이 다시 2x2가 될 수 있으므로 다시 순회
-      success(graph)
-      k = 0
-    } else {
-      k += 1
     }
+
+    // 2x2가 없는 경우
+    if (!blocks.length) break
+
+    // 시작 위치 기준으로 2x2 없애기
+    blocks.forEach(([i, j]) => {
+      graph[i][j] = 0
+      graph[i + 1][j] = 0
+      graph[i][j + 1] = 0
+      graph[i + 1][j + 1] = 0
+    })
+
+    // 재정렬
+    removeBoard(graph, m, n)
   }
+  // 0 개수 세기
+  answer = graph
+    .map((block) => block.filter((x) => !x).length)
+    .reduce((acc, cur) => acc + cur, 0)
 
   return answer
 }
