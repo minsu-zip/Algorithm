84aa2d1c69a5ac21ab6243a3bc2f7399caee871b
minsu-zip <alstn4814@gmail.com>
2022-06-01 23:53:11 +0900

    refactor : 이분탐색 알고리즘 사용

--- baekjoon/BinarySearch/11663_선분 위의 점.py
+++ baekjoon/BinarySearch/11663_선분 위의 점.py
@@ -1,3 +1,4 @@
+from bisect import bisect_left, bisect_right
 import sys
 input = sys.stdin.readline
 n, m = list(map(int, input().split()))
@@ -10,31 +11,10 @@ for _ in range(m):
 
 def solution():
     for [x, y] in mArr:
-        start = 0
-        end = n-1
-        # x점의 좌표 구하기
-        while start <= end:
-            mid = (start+end) // 2
-            if x > nArr[mid]:
-                start = mid + 1
-            else:
-                end = mid - 1
-        # 인덱스 포함 시작 지점
-        answerX = start
-
-        start = 0
-        end = n-1
-        # y점의 좌표 구하기
-        while start <= end:
-            mid = (start+end) // 2
-            if y >= nArr[mid]:
-                start = mid + 1
-            else:
-                end = mid - 1
-        # 인덱스 포함 마지막 지점
-        answerY = start
-        print('y x', answerY, answerX)
-        print(answerY - answerX)
+        # 이분탐색 자료구조 사용
+        start = bisect_left(nArr, x)
+        end = bisect_right(nArr, y)
+        print(end - start)
 
 
 solution()
