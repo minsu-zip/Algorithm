f7463d58066a3c8d7600a94b97f78299a627d823
minsu-zip <alstn4814@gmail.com>
2022-06-02 16:28:45 +0900

    refactor : 파이썬 이분탐색 자료구조 사용

--- baekjoon/BinarySearch/19637_IF문 좀 대신 써줘.py
+++ baekjoon/BinarySearch/19637_IF문 좀 대신 써줘.py
@@ -1,27 +1,21 @@
+from bisect import bisect_left
 import sys
 input = sys.stdin.readline
 n, m = list(map(int, input().split()))
-nArr = []
+tier = []
+rank = []
 for _ in range(n):
-    tier, rank = input().split()
-    nArr.append([tier, int(rank)])
-arr = sorted(nArr, key=lambda x: x[1])
+    a, b = input().split()
+    tier.append(a)
+    rank.append(int(b))
 mArr = [int(input()) for _ in range(m)]
 
 
 def solution():
-    for tier in mArr:
-        start = 0
-        end = n-1
-        answer = 0
-        while start <= end:
-            mid = (start+end) // 2
-            if tier <= arr[mid][1]:
-                answer = mid
-                end = mid - 1
-            else:
-                start = mid + 1
-        print(arr[answer][0])
+    for i in mArr:
+        # 파이썬 이분탐색 알고리즘
+        index = bisect_left(rank, i)
+        print(tier[index])
 
 
 solution()
